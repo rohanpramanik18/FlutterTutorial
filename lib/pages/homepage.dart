5d1bf02fbc4e5c5ecb671c39c240f1a9409af179
Rohan Pramanik <rohan.pramanik01@gmail.com>
2022-01-06 19:53:40 +0530

    CatalogGridViewComplete

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -36,11 +36,38 @@ class _HomePageState extends State<HomePage> {
           child: Text("Rohan App"),
         ),
       ),
-      body: ListView.builder(
+      body: GridView.builder(
         itemCount: CatalogModel.items.length,
         itemBuilder: (context, index) {
-          return ItemWidget(item: CatalogModel.items[index]);
+          return Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              clipBehavior: Clip.antiAlias,
+              elevation: 10,
+              child: GridTile(
+                child: Image.network(CatalogModel.items[index].image),
+                header: Container(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Text(
+                    CatalogModel.items[index].name,
+                    textAlign: TextAlign.center,
+                  ),
+                  color: Theme.of(context).primaryColor,
+                  height: 30,
+                ),
+                footer: Container(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Text(
+                    "\$${CatalogModel.items[index].price}",
+                    textAlign: TextAlign.center,
+                  ),
+                  color: Colors.red,
+                  height: 30,
+                ),
+              ));
         },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 13),
       ),
       drawer: MyDrawer(),
     );
